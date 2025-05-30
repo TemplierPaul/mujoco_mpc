73e4f8d48d1278131a009a0db33f15bbed3f5eb6
TemplierPaul <templier.paul@gmail.com>
2025-05-30 14:09:05 +0000

    fixed build error

--- mjpc/planners/replace_cem/planner.cc
+++ mjpc/planners/replace_cem/planner.cc
@@ -25,7 +25,7 @@
 #include <mujoco/mujoco.h>
 #include "mjpc/array_safety.h"
 #include "mjpc/planners/planner.h"
-#include "mjpc/planners/cross_entropy/planner.h"
+#include "mjpc/planners/sampling/planner.h"
 #include "mjpc/spline/spline.h"
 #include "mjpc/states/state.h"
 #include "mjpc/task.h"
@@ -37,6 +37,134 @@ namespace mjpc {
 
 namespace mju = ::mujoco::util_mjpc;
 using mjpc::spline::TimeSpline;
+
+
+// initialize data and settings
+void ReplaceCEMPlanner::Initialize(mjModel* model, const Task& task) {
+  // delete mjData instances since model might have changed.
+  data_.clear();
+
+  // allocate one mjData for nominal.
+  ResizeMjData(model, 1);
+
+  // model
+  this->model = model;
+
+  // task
+  this->task = &task;
+
+  // sampling noise
+  std_initial_ =
+      GetNumberOrDefault(0.1, model,
+                         "sampling_exploration");         // initial variance
+  std_min_ = GetNumberOrDefault(0.01, model, "std_min");  // minimum variance
+  // fraction of the trajectories that will use full exploration noise
+  explore_fraction_ =
+      GetNumberOrDefault(0.0, model, "explore_fraction");
+
+  // set number of trajectories to rollout
+  num_trajectory_ = GetNumberOrDefault(10, model, "sampling_trajectories");
+
+  // set number of elite samples max(best 10%, 2)
+  n_elite_ =
+      GetNumberOrDefault(std::max(num_trajectory_ / 10, 2), model, "n_elite");
+
+  if (num_trajectory_ > kMaxTrajectory) {
+    mju_error_i("Too many trajectories, %d is the maximum allowed.",
+                kMaxTrajectory);
+  }
+}
+
+// allocate memory
+void ReplaceCEMPlanner::Allocate() {
+  // initial state
+  int num_state = model->nq + model->nv + model->na;
+
+  // state
+  state.resize(num_state);
+  mocap.resize(7 * model->nmocap);
+  userdata.resize(model->nuserdata);
+
+  // policy
+  int num_max_parameter = model->nu * kMaxTrajectoryHorizon;
+  policy.Allocate(model, *task, kMaxTrajectoryHorizon);
+  resampled_policy.Allocate(model, *task, kMaxTrajectoryHorizon);
+  previous_policy.Allocate(model, *task, kMaxTrajectoryHorizon);
+
+  // scratch
+  parameters_scratch.resize(num_max_parameter);
+  times_scratch.resize(kMaxTrajectoryHorizon);
+
+  // noise
+  noise.resize(kMaxTrajectory * (model->nu * kMaxTrajectoryHorizon));
+
+  // variance
+  variance.resize(model->nu * kMaxTrajectoryHorizon);  // (nu * horizon)
+
+  // need to initialize an arbitrary order of the trajectories
+  trajectory_order.resize(kMaxTrajectory);
+  for (int i = 0; i < kMaxTrajectory; i++) {
+    trajectory_order[i] = i;
+  }
+
+  // trajectories and parameters
+  for (int i = 0; i < kMaxTrajectory; i++) {
+    trajectory[i].Initialize(num_state, model->nu, task->num_residual,
+                             task->num_trace, kMaxTrajectoryHorizon);
+    trajectory[i].Allocate(kMaxTrajectoryHorizon);
+    candidate_policy[i].Allocate(model, *task, kMaxTrajectoryHorizon);
+  }
+  nominal_trajectory.Initialize(num_state, model->nu, task->num_residual,
+                                task->num_trace, kMaxTrajectoryHorizon);
+  nominal_trajectory.Allocate(kMaxTrajectoryHorizon);
+}
+
+// reset memory to zeros
+void ReplaceCEMPlanner::Reset(int horizon,
+                                const double* initial_repeated_action) {
+  // state
+  std::fill(state.begin(), state.end(), 0.0);
+  std::fill(mocap.begin(), mocap.end(), 0.0);
+  std::fill(userdata.begin(), userdata.end(), 0.0);
+  time = 0.0;
+
+  // policy parameters
+  policy.Reset(horizon, initial_repeated_action);
+  resampled_policy.Reset(horizon, initial_repeated_action);
+  previous_policy.Reset(horizon, initial_repeated_action);
+
+  // scratch
+  std::fill(parameters_scratch.begin(), parameters_scratch.end(), 0.0);
+  std::fill(times_scratch.begin(), times_scratch.end(), 0.0);
+
+  // noise
+  std::fill(noise.begin(), noise.end(), 0.0);
+
+  // variance
+  double var = std_initial_ * std_initial_;
+  std::fill(variance.begin(), variance.end(), var);
+
+  // trajectory samples
+  for (int i = 0; i < kMaxTrajectory; i++) {
+    trajectory[i].Reset(kMaxTrajectoryHorizon);
+    candidate_policy[i].Reset(horizon);
+  }
+  nominal_trajectory.Reset(kMaxTrajectoryHorizon);
+
+  for (const auto& d : data_) {
+    mju_zero(d->ctrl, model->nu);
+  }
+
+  // improvement
+  improvement = 0.0;
+}
+
+// set state
+void ReplaceCEMPlanner::SetState(const State& state) {
+  state.CopyTo(this->state.data(), this->mocap.data(), this->userdata.data(),
+               &this->time);
+}
+
 
 // optimize nominal policy using random sampling
 void ReplaceCEMPlanner::OptimizePolicy(int horizon, ThreadPool& pool) {
@@ -181,6 +309,19 @@ void ReplaceCEMPlanner::NominalTrajectory(int horizon, ThreadPool& pool) {
   NominalTrajectory(horizon);
 }
 
+
+// set action from policy
+void ReplaceCEMPlanner::ActionFromPolicy(double* action, const double* state,
+                                           double time, bool use_previous) {
+  const std::shared_lock<std::shared_mutex> lock(mtx_);
+  if (use_previous) {
+    previous_policy.Action(action, state, time);
+  } else {
+    policy.Action(action, state, time);
+  }
+}
+
+
 // update policy via resampling
 void ReplaceCEMPlanner::ResamplePolicy(int horizon) {
   // dimensions
@@ -303,5 +444,134 @@ void ReplaceCEMPlanner::Rollouts(int num_trajectory, int horizon,
   // wait
   pool.WaitCount(count_before + num_trajectory + 1);
   pool.ResetCount();
+}
+
+
+// returns the **nominal** trajectory (this is the purple trace)
+const Trajectory* ReplaceCEMPlanner::BestTrajectory() {
+  return &nominal_trajectory;
+}
+
+// visualize planner-specific traces
+void ReplaceCEMPlanner::Traces(mjvScene* scn) {
+  // sample color
+  float color[4];
+  color[0] = 1.0;
+  color[1] = 1.0;
+  color[2] = 1.0;
+  color[3] = 1.0;
+
+  // width of a sample trace, in pixels
+  double width = GetNumberOrDefault(3, model, "agent_sample_width");
+
+  // scratch
+  double zero3[3] = {0};
+  double zero9[9] = {0};
+
+  // best
+  auto best = this->BestTrajectory();
+
+  // sample traces
+  int n_elite = n_elite_;
+  for (int k = 0; k < n_elite; k++) {
+    // plot sample
+    for (int i = 0; i < best->horizon - 1; i++) {
+      if (scn->ngeom + task->num_trace > scn->maxgeom) break;
+      for (int j = 0; j < task->num_trace; j++) {
+        // initialize geometry
+        mjv_initGeom(&scn->geoms[scn->ngeom], mjGEOM_LINE, zero3, zero3, zero9,
+                     color);
+
+        // elite index
+        int idx = trajectory_order[k];
+        // make geometry
+        mjv_connector(
+            &scn->geoms[scn->ngeom], mjGEOM_LINE, width,
+            trajectory[idx].trace.data() + 3*task->num_trace * i + 3 * j,
+            trajectory[idx].trace.data() + 3*task->num_trace * (i + 1) + 3 * j);
+
+        // increment number of geometries
+        scn->ngeom += 1;
+      }
+    }
+  }
+}
+
+// planner-specific GUI elements
+void ReplaceCEMPlanner::GUI(mjUI& ui) {
+  mjuiDef defCrossEntropy[] = {
+      {mjITEM_SLIDERINT, "Rollouts", 2, &num_trajectory_, "0 1"},
+      {mjITEM_SELECT, "Spline", 2, &interpolation_,
+       "Zero\nLinear\nCubic"},
+      {mjITEM_SLIDERINT, "Spline Pts", 2, &policy.num_spline_points, "0 1"},
+      {mjITEM_SLIDERNUM, "Init. Std", 2, &std_initial_, "0 1"},
+      {mjITEM_SLIDERNUM, "Min. Std", 2, &std_min_, "0.01 0.5"},
+      {mjITEM_SLIDERNUM, "Explore", 2, &explore_fraction_, "0.0 1.0"},
+      {mjITEM_SLIDERINT, "Elite", 2, &n_elite_, "2 128"},
+      {mjITEM_END}};
+
+  // set number of trajectory slider limits
+  mju::sprintf_arr(defCrossEntropy[0].other, "%i %i", 1, kMaxTrajectory);
+
+  // set spline point limits
+  mju::sprintf_arr(defCrossEntropy[2].other, "%i %i", MinSamplingSplinePoints,
+                   MaxSamplingSplinePoints);
+
+  // set noise standard deviation limits
+  mju::sprintf_arr(defCrossEntropy[3].other, "%f %f", MinNoiseStdDev,
+                   MaxNoiseStdDev);
+
+  // add cross entropy planner
+  mjui_add(&ui, defCrossEntropy);
+}
+
+// planner-specific plots
+void ReplaceCEMPlanner::Plots(mjvFigure* fig_planner, mjvFigure* fig_timer,
+                                int planner_shift, int timer_shift,
+                                int planning, int* shift) {
+  // ----- planner ----- //
+  double planner_bounds[2] = {-6.0, 6.0};
+
+  // improvement
+  mjpc::PlotUpdateData(fig_planner, planner_bounds,
+                       fig_planner->linedata[0 + planner_shift][0] + 1,
+                       mju_log10(mju_max(improvement, 1.0e-6)), 100,
+                       0 + planner_shift, 0, 1, -100);
+
+  // legend
+  mju::strcpy_arr(fig_planner->linename[0 + planner_shift], "Avg - Best");
+
+  fig_planner->range[1][0] = planner_bounds[0];
+  fig_planner->range[1][1] = planner_bounds[1];
+
+  // bounds
+  double timer_bounds[2] = {0.0, 1.0};
+
+  // ----- timer ----- //
+
+  PlotUpdateData(
+      fig_timer, timer_bounds, fig_timer->linedata[0 + timer_shift][0] + 1,
+      1.0e-3 * noise_compute_time * planning, 100, 0 + timer_shift, 0, 1, -100);
+
+  PlotUpdateData(fig_timer, timer_bounds,
+                 fig_timer->linedata[1 + timer_shift][0] + 1,
+                 1.0e-3 * rollouts_compute_time * planning, 100,
+                 1 + timer_shift, 0, 1, -100);
+
+  PlotUpdateData(fig_timer, timer_bounds,
+                 fig_timer->linedata[2 + timer_shift][0] + 1,
+                 1.0e-3 * policy_update_compute_time * planning, 100,
+                 2 + timer_shift, 0, 1, -100);
+
+  // legend
+  mju::strcpy_arr(fig_timer->linename[0 + timer_shift], "Noise");
+  mju::strcpy_arr(fig_timer->linename[1 + timer_shift], "Rollout");
+  mju::strcpy_arr(fig_timer->linename[2 + timer_shift], "Policy Update");
+
+  // planner shift
+  shift[0] += 1;
+
+  // timer shift
+  shift[1] += 3;
 }
 }  // namespace mjpc
